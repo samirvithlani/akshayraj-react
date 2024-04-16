361ad32954f88f39ac44fe86bf1b7cab5368521f
samirvithlani <samir.vithlani83955@gmail.com>
2024-04-16 14:08:58 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HomeComponent } from "./components/HomeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import { MapDemo } from "./components/MapDemo";
 import { Content } from "./components/Content";
+import { UseStateDemo } from "./components/UseStateDemo";
 
 function App() {
 
@@ -18,7 +19,8 @@ function App() {
   return (
     <div className="App">
      <HeaderComponent title = {title}></HeaderComponent>
-     <Content t = {title} s = {style}></Content>
+     {/* <Content t = {title} s = {style}></Content> */}
+     <UseStateDemo></UseStateDemo>
       {/* <HomeComponent></HomeComponent> */}
       {/* <MapDemo></MapDemo> */}
     </div>

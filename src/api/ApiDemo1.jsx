0dcce4224ee52f7462292e18e9d5071a31ac85ee
samirvithlani <samir.vithlani83955@gmail.com>
2024-05-06 13:28:15 +0530

    delete demo

--- src/api/ApiDemo1.jsx
+++ src/api/ApiDemo1.jsx
@@ -1,36 +1,103 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { CutomeLoader } from "../components/CutomeLoader";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ApiDemo1 = () => {
+  const [users, setusers] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const getApiDemo1 = async () => {
+    //api -->data
+    setisLoading(true);
+    const res = await axios.get("https://node5.onrender.com/user/user");
+    console.log("res", res);
+    console.log("res.data.message", res.data.message);
+    console.log("res.data.data", res.data.data);
+    setusers(res.data.data);
+    setisLoading(false);
+  };
 
-    const [users, setusers] = useState([])
-    const getApiDemo1 = async () => {
-        //api -->data
-
-        const res = await axios.get("https://node5.onrender.com/user/user")
-        console.log("res",res)
-        console.log("res.data.message",res.data.message)
-        console.log("res.data.data",res.data.data)
-        setusers(res.data.data)
-
-
+  const deleteUser = async (id) => {
+    const res = await axios.delete(
+      "https://node5.onrender.com/user/user/" + id
+    );
+    console.log("res", res);
+    if (res.status === 204) {
+      //alert("User Deleted Successfully");
+      toast.error('User Deleted SuccessFully...', {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        
+        });
+      getApiDemo1();
     }
+  };
+
   return (
     <div>
-        <h1>API DEMO 1</h1>
-        <button onClick={()=>{getApiDemo1()}}>GET</button>
-        <ul>
-        {
-            users?.map((user)=>{
-                return(
-                    <li>
-                        {user._id} - {user.name} - {user.email}
-                    </li>
-                )
-            })
-        }
-        </ul>
-
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <h1>API DEMO 1</h1>
+      <button
+        onClick={() => {
+          getApiDemo1();
+        }}
+      >
+        GET
+      </button>
+      {isLoading && <CutomeLoader />}
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>AGE</th>
+            <th>STATUS</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user) => {
+            return (
+              <tr>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
+                <td>{user.isActive == true ? "Active" : "Not Active"}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteUser(user._id);
+                    }}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
---
+++ src/components/CutomeLoader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../assets/css/loader.css'
+
+export const CutomeLoader = () => {
+  return (
+    <div class="spinner-container">
+    <div class="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  
+  );
+};
